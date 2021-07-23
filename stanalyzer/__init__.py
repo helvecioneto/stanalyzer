0d3ebba2e0cb43b2f1aea1ae8c9a3fc0052adf31
Helvecio Leal <helvecio.neto@inpe.br>
2021-07-23 11:11:46 -0300

    Refactor

--- stanalyzer/__init__.py
+++ stanalyzer/__init__.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 from .read import read_file
 from .duration import life_cicle
-=======
-from .read import read_file
->>>>>>> 985ec4783ef6089edeed3a11f03bbaf04e94318a
--- stanalyzer/read.py
+++ stanalyzer/read.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 def read_file(path=None):
-<<<<<<< HEAD
     '''
     This function is used for reading the tracking files (.pkl type) from Fortracc2.
 
@@ -15,6 +14,3 @@ def read_file(path=None):
         return pd.read_pickle(path)
     except:
         print('File not found or path parameter is incorrect.') 
-=======
-    return pd.read_pickle(path)
->>>>>>> 985ec4783ef6089edeed3a11f03bbaf04e94318a
